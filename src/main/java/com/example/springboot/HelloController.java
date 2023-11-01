0ade05ce69655274862e965238802927a1f5d236
Mark Fynes <tfynes@vmware.com>
2023-11-01 15:20:35 -0400

    .

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -4,10 +4,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
-
-
-
 @RestController
 public class HelloController {
 
