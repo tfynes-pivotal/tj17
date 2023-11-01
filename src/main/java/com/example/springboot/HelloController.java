08c0854362efd36b471e4bb6a42a19774789474a
Mark Fynes <tfynes@vmware.com>
2023-11-01 15:07:57 -0400

    foovar

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -3,12 +3,17 @@ package com.example.springboot;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+@Value("${foo}")
+private String foo;
+
+
 @RestController
 public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Tanzu!";
+		return "Tanzu! " + foo;
 	}
 
 }
