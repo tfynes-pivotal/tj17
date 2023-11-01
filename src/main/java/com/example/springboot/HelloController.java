bfe4bdf400ac998ee156fdbc3d78b9da31f99bcc
Mark Fynes <tfynes@vmware.com>
2023-11-01 15:14:18 -0400

    .

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,15 +1,21 @@
 package com.example.springboot;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
-@Value("${foo}")
-private String foo;
+
 
 
 @RestController
 public class HelloController {
+
+   @Value("${foo}")
+   private String foo;
+
+
+
 
 	@RequestMapping("/")
 	public String index() {
