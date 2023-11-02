9926eea984db36d063371ad77f22787a98e6dddc
Mark Fynes <tfynes@vmware.com>
2023-11-02 16:18:45 -0400

    .

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController {
 
-   @Value("${dsc.salt}")
+   @Value("${dsc}")
    private String foo;
 
 
